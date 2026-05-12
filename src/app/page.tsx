a1b3495627b86b7310f90ba82589ea7b6bbdad0d
Michas <michaelcalb.dev@gmail.com>
2026-05-12 18:10:56 -0300

    work in progress note

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,6 +17,7 @@ export default function Home() {
 				<Image src='/200.png' width={128} height={128} alt='Logo' />
 				<h1 className={styles.title}>10cyphr</h1>
 			</header>
+            <span className={styles.note}>This is still a work in progress, some decoders may not be fully implemented yet, or not implemented at all.</span>
 			<main className={styles.main}>
 				<fieldset className={styles.fieldset}>
                     <legend className={styles.legend}>EZdecoders</legend>
