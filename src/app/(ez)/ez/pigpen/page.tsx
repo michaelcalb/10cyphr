56c699cb3ad8e89d5818a73a343ad42ede86fd2a
Michas <michaelcalb.dev@gmail.com>
2026-05-14 11:10:00 -0300

    pigpen update

--- src/app/(ez)/ez/pigpen/page.tsx
+++ src/app/(ez)/ez/pigpen/page.tsx
@@ -4,44 +4,61 @@ import clsx from 'clsx'
 import styles from './page.module.css'
 import { useState } from 'react'
 
-const tictactoeChars = [
-    'abcdefghi', 'jklmnopqr'
-]
-const tictactoeLength = tictactoeChars[0].length
+const tictactoeChars = ['abcdefghi', 'jklmnopqr']
 
-const xChars = [
-    'sutv', 'wyxz'
-]
-const xLength = xChars[0].length
+const xChars = ['sutv', 'wyxz']
 
 export default function Pigpen() {
-    const [output, setOutput] = useState('')
+	const [output, setOutput] = useState('')
 
-    const handleClick = (char: string) => {
-        setOutput(prev => prev + char)
-    }
+	const handleClick = (char: string) => {
+		setOutput((prev) => prev + char)
+	}
 
-    const handleDoubleClick = (char: string) => {
-        setOutput(prev => prev.slice(0, -2))
-        setOutput(prev => prev + char)
-    }
+	const handleDoubleClick = (char: string) => {
+		setOutput((prev) => prev.slice(0, -2))
+		setOutput((prev) => prev + char)
+	}
 
-    return (
-        <main className={styles.main}>
-            <div className={styles.pigpen}>
-                <div className={styles.tictactoe}>
-                    {tictactoeChars[0].split('').map((char, i) => (
-                        <button key={i} className={clsx(styles.btn, styles[`tttCell${i}`])} onClick={() => handleClick(char)} onDoubleClick={() => handleDoubleClick(tictactoeChars[1][i])}></button>
-                    ))}
-                </div>
-                <div className={styles.x}>
-                    {xChars[0].split('').map((char, i) => (
-                        <button key={i} className={clsx(styles.btn, styles[`xCell${i}`])} onClick={() => handleClick(char)} onDoubleClick={() => handleDoubleClick(xChars[1][i])}></button>
-                    ))}
-                </div>
-            </div>
-            <input type="text" value={output} readOnly />
-            <button onClick={() => setOutput('')}>Clear</button>
-        </main>
-    )
+	const handleDelete = () => {
+		setOutput((prev) => prev.slice(0, -1))
+	}
+
+	const handleClear = () => {
+		setOutput('')
+	}
+
+	return (
+		<main className={styles.main}>
+			<div className={styles.pigpen}>
+				<div className={styles.tictactoe}>
+					{tictactoeChars[0].split('').map((char, i) => (
+						<button
+							key={i}
+							className={clsx(styles.btn, styles[`tttCell${i}`])}
+							onClick={() => handleClick(char)}
+							onDoubleClick={() =>
+								handleDoubleClick(tictactoeChars[1][i])
+							}
+						></button>
+					))}
+				</div>
+				<div className={styles.x}>
+					{xChars[0].split('').map((char, i) => (
+						<button
+							key={i}
+							className={clsx(styles.btn, styles[`xCell${i}`])}
+							onClick={() => handleClick(char)}
+							onDoubleClick={() =>
+								handleDoubleClick(xChars[1][i])
+							}
+						></button>
+					))}
+				</div>
+			</div>
+			<input type='text' value={output} readOnly />
+			<button onClick={handleDelete}>Delete</button>
+			<button onClick={handleClear}>Clear</button>
+		</main>
+	)
 }
