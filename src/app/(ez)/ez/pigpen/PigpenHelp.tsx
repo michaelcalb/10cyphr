442421f7578d05bb17941859f6b5c086c957b3c7
Michas <michaelcalb.dev@gmail.com>
2026-06-04 15:27:54 -0300

    pigpen help

--- src/app/(ez)/ez/pigpen/PigpenHelp.tsx
+++ src/app/(ez)/ez/pigpen/PigpenHelp.tsx
@@ -1,6 +1,19 @@
+import Image from "next/image";
+import styles from './page.module.css'
+
 export default function PigpenHelp() {
 
     return (
-        <p>ezpigpen tutorial</p>
+        <>
+            <Image src="/pigpen_guide.png" alt="pigpen guide" width={400} height={400} className={styles.helpImg}/>
+            <div className={styles.helpSingleClick}>
+                <span className={styles.helpDrawing}></span>
+                <span className={styles.helpResult}>= Single Click</span>
+            </div>
+            <div className={styles.helpDoubleClick}>
+                <span className={styles.helpDrawing}></span>
+                <span className={styles.helpResult}>= Double Click</span>
+            </div>
+        </>
     )
 }
